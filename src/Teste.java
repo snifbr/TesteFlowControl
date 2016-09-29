1e9d500e0ac528b0244f2c6a0280d78fee4ed576
esoft15 - Danilo Figueiredo Rocha <drocha.figueiredo@gmail.com>
2016-09-29 03:13:21 +0000

    Exercicio Lista3

---
+++ src/Teste.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+	
+	void forTestMethod() {
+		for (int i = 0; i <= 10; i++) {
+			for (int j = 10; j < i; j--) {
+				if (i == j) {
+					break;
+				} else {
+					System.out.println(i + " + " + j);;
+				}
+			}	
+		}
+		
+	}
+	
+	void separarParImpar(ArrayList<Integer> lista) {
+		ArrayList<Integer> pares = null,impares = null;
+		Integer i = 0;
+		if (lista != null) {
+			while (i < lista.size()) {
+				switch (lista.get(i) % 2) {
+				case 0:
+					pares.add(lista.get(i));
+					break;
+				case 1:
+					impares.add(lista.get(i));
+				default:
+					break;
+				}
+				i++;
+			}
+			System.out.println("Pares: " + pares + " Impares: " + impares);
+		} else {
+			System.out.println("Lista de numeros invalida.");
+		}
+		System.out.println("Fim");
+	}
+
+}
